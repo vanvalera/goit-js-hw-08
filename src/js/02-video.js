3e32da9016cc36042cd31322b3830ce3b84b5327
vanvalera <vmarton777@gmail.com>
2023-04-15 11:32:47 +0300

    fix_bug

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,2 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
-const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
-
-player.on('timeupdate', throttle(playerTimeUpdate, 1000));
-
-function playerTimeUpdate(data) {
-  const currentTime = data.seconds;
-
-  localStorage.setItem('videoplayer-current-time', currentTime);
-}
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
