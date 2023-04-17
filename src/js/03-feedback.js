6e320590737f70465ed62719096d9db9543e8be3
vanvalera <vmarton777@gmail.com>
2023-04-17 23:24:45 +0300

    complete_task_3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  email: document.querySelector('[name="email"]'),
+  textarea: document.querySelector('[name="message"]'),
+  form: document.querySelector('form'),
+};
+
+const STORAGE_KEY = 'feedback-form-state';
+let feedbackData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+
+function recoverData() {
+  refs.email.value = feedbackData.email || '';
+  refs.textarea.value = feedbackData.message || '';
+}
+
+recoverData();
+
+function updateFeedbackData(event) {
+  feedbackData[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackData));
+}
+function onFormSubmitting(event) {
+  event.preventDefault();
+
+  console.log(feedbackData);
+
+  localStorage.removeItem(STORAGE_KEY);
+  event.currentTarget.reset();
+  feedbackData = {};
+}
+
+refs.form.addEventListener('input', throttle(updateFeedbackData, 500));
+refs.form.addEventListener('submit', onFormSubmitting);
